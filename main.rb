6d0756957fb333d252d7d265a3fb9959e26800d3
Gabriel de Tassigny <gabriel@multivac.cc>
2015-05-03 00:15:30 +0100

    Set the downloaded image as wallpaper

--- main.rb
+++ main.rb
@@ -3,4 +3,10 @@
 require './downloader.rb'
 
 dl = Downloader.new
-dl.get_top_link
+filename = dl.get_top_link
+
+if filename.nil?
+  puts "Error : could not download any wallpaper!"
+else
+  `osascript set_wallpaper.scpt #{filename}`
+end
